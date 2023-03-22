f7b130972b79220aa8325c5477545a04811fada2
Jess Leyva <jessica.leyvat@yahoo.com>
2023-03-22 11:15:44 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(names,event){
+    
+    for(let i=0; i<names.length;i++) {
+    
+     let customMsg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+     messages.push(customMsg)
+    }
+    return messages; 
+}
+function countDown(number){
+    while(number > -1){
+    console.log(number);
+    number-- ;
+    }
+}
+
+ 
+
